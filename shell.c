d7b12ed8fa0faedc28bbc4558c1fd5f254b50064
chronosss1 <50720898+chronosss1@users.noreply.github.com>
2020-04-22 02:00:11 +0200

    Add files via upload
    shell rehecha, ahora funciona llamando al programa del que metes el nombre y usa mas o menos solo lo necesario

--- shell.c
+++ shell.c
@@ -1,76 +1,18 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <time.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <dirent.h>
-#include <pwd.h>
-#include <grp.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <errno.h>
-#include <sys/mman.h>
-#include <ctype.h>
-#include <sys/wait.h>
+#include <stdio.h> //por : printf,perror,stdin
+#include <string.h> // por el strok el
+#include <unistd.h> //por el execvp y fork
+//#include <fcntl.h> //para el dup hará falta
+#include <sys/wait.h> //por el waitpid
+#include <stdlib.h> //por el exit()
 
 #include "provisional.h"
 
 /*************************************************************************************************/
 
 #define MAXLINE 2048
-#define MAXNAME 1024
-#define MAXARRAY 4096
-
-struct COMMAND{
-	char *name;
-	void(*pfunc)(char *tr[]);
-};
-
-void CmdCdir(char * tr[]){
-	if (tr[0] == NULL)
-		printf("%s\n", getcwd(tr[0],MAXNAME));
-	else
-		if (chdir(tr[0])==-1)
-			perror("Cannot change directory");
-}
-
-void CmdPid (char *tr[]){
-	if (tr[0] !=NULL && strcmp(tr[0],"-p")==0)
-		printf("parent proces pid %d\n",getpid());
-	else
-		printf("process pid: %d\n",getpid());
-}
-
-void CmdFecha (){
-	time_t t;
-	struct tm *tm;
-	char date[100];
-
-	t=time(NULL);
-	tm=localtime(&t);
-	if (t && tm){
-		strftime(date,100,"%d / %m / %Y",tm);
-		printf("Today is %s\n",date);
-	}else {perror("error, time couldn`t be collected");}
-}
 
 void CmdExit(){
 	exit(0);
-}
-
-void CmdHora (){
-	time_t t;
-	struct tm *tm;
-	char date[100];
-
-	t=time(NULL);
-	tm=localtime(&t);
-	if (t && tm){
-	strftime(date,100,"%H:%M:%S",tm);
-	printf("It is %s\n",date);}
-	else {perror("error, time couldn`t be collected");}
 }
 
 int TrocearCadena (char * cadena, char * trozos[]){
@@ -92,43 +34,57 @@ void CmdNotFound (char * tr[]){
 	}
 	printf (" not found \n");
 }
-
+/*
 void CmdDup(char * tr[]){
 	int i;
 	if ((i = atoi(tr[0]) != 0))
 		dupNuevo(i);
 
 }
+*/
+void Cmd_exec(char *tr[]){
+	if (tr[0] != NULL ){
 
+			execvp(tr[0],tr);
+	}
+}
 
-struct COMMAND cmd []={
-	{"pid",CmdPid},
-	{"cdir",CmdCdir},
-	{"fecha",CmdFecha},
-	{"hora",CmdHora},
-	{"date",CmdFecha},
-	{"time",CmdHora},
-	{"fin",CmdExit},
-	{"end",CmdExit},
-	{"exit",CmdExit},
-    /*********************************************************************************************/
-    {"dup",CmdDup},
-	{NULL,NULL}
-};
+int pplano(char *tr[]){
+	pid_t p = -1;
+	if (tr[0] != NULL ){
+		if (*tr[0] == '@') {
+			if (tr[1] != NULL){
+				if((p=fork()) == -1)
+					perror("Error cant fork");
+				else if (p == 0) {
+				printf("aaa");
+					Cmd_exec(tr);
+				}
+				else waitpid(p,NULL,0);
+			}
+		}
+		else {
+			if((p=fork()) == -1)
+				perror("Error cant fork");
+			else if (p == 0) {
+				Cmd_exec(tr);
+			}
+			else waitpid(p,NULL,0);
+		}
+	}
+	return p;
+}	
+
+void Cmd_pplano(char *tr[]){
+	pplano(tr);
+}
 
 void ProcessInput (char imp[]){
 	char *tr[MAXLINE/2];
-	int i;
 	if(TrocearCadena(imp, tr)==0){
 		return;
 	}
-	for (i=0; cmd[i].name!=NULL;i++){
-		if (strcmp(tr[0],cmd[i].name)==0){
-			(*cmd[i].pfunc)(tr+1);
-			return;
-		}
-	}
-	CmdNotFound(tr); //antes: CmdNotFound(tr);
+	Cmd_pplano(tr);
 }
 
 
@@ -140,8 +96,8 @@ int main (int argc, char *argv[]){
 
 	while (1){
 		printf("#) ");
-		//printf("%lu",sizeof(struct node)); 56
 		fgets (input,MAXLINE,stdin);
 		ProcessInput (input);
+
 	}
 }
